30882072ecc338d6cf373183d055733b51f2b44f
rov-machine <o5ve2gdv@duck.com>
2022-12-26 15:47:32 -0500

    updated to send serialized dictionary instead of string

--- socket/ws_client.py
+++ socket/ws_client.py
@@ -12,41 +12,41 @@ import asyncio
 import websockets
 import sqlite3
 import re
+import json
+import socket
+
+SERVER_IP = "ws://" + socket.gethostbyname('rov.local') + ":5000"
+print("Connecting to:", SERVER_IP)
 
 con = sqlite3.connect("rov_local.db")
 cur = con.cursor()
 
-
-
 #for row in cur.execute('SELECT * from data'):
 #    print(row)
 
-def insert_to_db(chamber_tempf, sensor_tempf, pressure_psi, pressure_ft):
+def insert_to_db(senddatetime, chamber_tempf, sensor_tempf, pressure_psi, pressure_ft):
     try:
-        cur.execute(f"INSERT INTO data VALUES (NULL, {chamber_tempf}, {sensor_tempf}, {pressure_psi}, {pressure_ft})")
+        cur.execute(f"INSERT INTO data VALUES (NULL, \'{senddatetime}\', {chamber_tempf}, {sensor_tempf}, {pressure_psi}, {pressure_ft})")
         con.commit()
     except sqlite3.OperationalError:
         print("No such table <data> .... Creating it now")
         create_db()
-        cur.execute(f"INSERT INTO data VALUES (NULL, {chamber_tempf}, {sensor_tempf}, {pressure_psi}, {pressure_ft})")
-        con.commit()
 
 def create_db():
     cur.execute("""
      CREATE TABLE data (
-         dataID integer PRIMARY KEY AUTOINCREMENT
-        ,chamber_tempF float
-        ,sensor_tempF float
-        ,pressure_psi float
-        ,pressure_ft floatx);
+          dataID integer PRIMARY KEY AUTOINCREMENT
+        , senddatetime text
+        , chamber_tempF float
+        , sensor_tempF float
+        , pressure_psi float
+        , pressure_ft floatx);
         """)
     con.commit()
 
 async def get_broadcast():
-    uri = "ws://192.168.1.142:5000"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(SERVER_IP) as websocket:
         message = await websocket.recv()
-        #print(f"ROV: {message}")
         return message
 
 
@@ -57,10 +57,10 @@ if __name__ == "__main__":
             #break
         #i += 1
         
-        # Get the broadcast message
+        # Get the broadcast message and de-serialize as dictionary
         message = asyncio.run(get_broadcast())
+        message = json.loads(message)
         print(message)
-        # Split the message to separate the values from the labels
-        split_message = re.split(r'[:,]', message)
-        # Insert that message into the local db
-        insert_to_db(split_message[1],split_message[3],split_message[5],split_message[7])
+
+        # Insert received data into local database
+        insert_to_db(message["senddatetime"], message["chamber_tempF"], message["sensor_tempF"], message["pressure_psi"], message["pressure_ft"])
--- socket/ws_server.py
+++ socket/ws_server.py
@@ -1,49 +1,62 @@
 
 import asyncio
-import datetime
-import random
+from datetime import datetime
 import websockets
+import json
 from sensors import BMP180
 from sensors import SSC
+import socket
 
+# Amount of time between each broadcast.
+BROADCAST_FREQ = 0.5
 
+MY_IP = socket.gethostbyname(socket.gethostname() + ".local")
+print("Running on:", MY_IP)
 
-
-
+# Instantiate chamber pressure sensor 
 tcouple = BMP180.BMP()
+# Instantiate wet media sensor
 press = SSC.SSC30()
+# Set websocket 
 CONNECTIONS = set()
 
 async def register(websocket):
+    # Keep track of all connections
     CONNECTIONS.add(websocket)
     try:
-        await websocket.wait_closed()
+        async for _ in websocket:
+            pass
     finally:
         CONNECTIONS.remove(websocket)
 
 
-async def show_time():
+async def send_message():
+    # Broadcast a message to all connections
     while True:
-        message = datetime.datetime.utcnow().isoformat() + "Z"
+        # Wait a set number of seconds before new broadcast as we
+        #   must yield control to the event loop between each message
+        await asyncio.sleep(BROADCAST_FREQ)
+        # Get current date/time and format it
+        dt = datetime.now()
+        sendtime = dt.strftime('%Y-%m-%d %X:%f')
+        # Create message dictionary
+        message = {
+              "senddatetime": sendtime
+            , "chamber_tempF": tcouple.get_tempF()
+            , "sensor_tempF": press.get_tempF()
+            , "pressure_psi": press.get_pressPSI()
+            , "pressure_ft": press.get_pressFT()
+        }
+        # Serialize dictionary so it can be broacast
+        message = json.dumps(message)
+        # Sent the message (Consider switching to concurrent in future)
         websockets.broadcast(CONNECTIONS, message)
-        await asyncio.sleep(random.random() * 2 + 1)
-
-async def show_temp():
-    while True:
-        message = str(
-            f"\nChamber Temp 'F: {tcouple.get_tempF()}"
-            f"\nSensor Temp 'F: {press.get_tempF()}"
-            f"\nPressure PSI: {press.get_pressPSI()}"
-            f"\nPressure FT: {press.get_pressFT()}"
-        )
         
-        websockets.broadcast(CONNECTIONS, message)
-        await asyncio.sleep(3)
 
 
 async def main():
-    async with websockets.serve(register, "192.168.1.142", 5000):
-        await show_temp()
+    async with websockets.serve(register, MY_IP, 5000):
+        await send_message()
 
 
 if __name__ == "__main__":
