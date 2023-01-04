724948c49d7b73b2003c4dd3b6827b2974bf016a
rov-machine <o5ve2gdv@duck.com>
2023-01-03 20:35:46 -0500

    Minimal working state, but a script

--- socket/sensors/LIS2MDL.py
+++ socket/sensors/LIS2MDL.py
@@ -6,14 +6,13 @@
 ## https://www.mouser.com/datasheet/2/389/lis2mdl-1849648.pdf
 ##
 #####
-#print(bin(a[1])[2:].zfill(8))
 
 import time
 from smbus2 import SMBus
 import struct
+import math
 
-
-# Register address map
+#region Register Map
 OFFSET_X_REG_L = 0x45
 OFFSET_X_REG_H = 0x46
 OFFSET_Y_REG_L = 0x47
@@ -35,82 +34,114 @@ TEMP_OUT_H_REG = 0x6F
 INT_CRTL_REG = 0x63
 INT_SOURCE_REG = 0x64
 INT_THS_L_REG = 0x65
+#endregion
 
-ADDRESS = 0x1E
-bus = SMBus(1)  
 
-def get_config():
-    print(f"Current CFG-A {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_A, 1)[0])[2:].zfill(8)}")
-    #print(f"Current CFG-B {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_B, 1)[0])[2:].zfill(8)}")
-    #print(f"Current CFG-C {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_C, 1)[0])[2:].zfill(8)}")
-    #print(f"Current STATUS {bin(bus.read_i2c_block_data(ADDRESS, STATUS_REG, 1)[0])[2:].zfill(8)}")
+class LIS2MDL:
+    _I2C_ADDRESS = 0x1E
+    _MAG_SCALE = 0.15
+    hardiron_calibration = [-7.334999999999999, 4.68], [-3.87, 3.1725], [-22.229999999999997, -14.355]
+    bus = SMBus(1) 
+    
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        # Set the sensor to default state. Soft reset
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0b00100000)
+        time.sleep(0.1)
+        # Reboot
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0b01000000)
+        time.sleep(0.1)
+        # Set mode to continuous
+        # Also enable temp compensation
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0b10000000)
+        # Set BDU to avoid reading incorrect data when reading asynchronously
+        # Also set interrupt signal to be driven on INT/DRDY pin
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_C, 0b01010000)
+        # Set interrupt bit to latched
+        # Also set polarity of interrupt bit (1 = interrupt)
+        self.bus.write_byte_data(self._I2C_ADDRESS, INT_CRTL_REG, 0b00000110)
+        # Set interrupt block recognition checks after hard-iron correction
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_B, 0b00001000)
+        # Let measurements stabilize
+        time.sleep(0.1)
+    
+    def their_reset(self):
+        # This can probably be deleted soon
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0b00100000)
+        time.sleep(0.1)
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0b01000000)
+        time.sleep(0.1)
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_A, 0xF1)
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_B, 0x08)
+        self.bus.write_byte_data(self._I2C_ADDRESS, CFG_REG_C, 0x50)
+        self.bus.write_byte_data(self._I2C_ADDRESS, INT_CRTL_REG, 0xE7)
+
+    def calibrate(self):
+        self.hardiron_calibration = [[1000, -1000], [1000, -1000], [1000, -1000]]
+        print("Calibrating in 3..")
+        time.sleep(1)
+        print("2...")
+        time.sleep(1)
+        print("1...")
+        time.sleep(1)
+        start_time = time.monotonic()
+        while time.monotonic() - start_time < 10.0:
+            magval = self.magnetic()
+            print("Calibrating - X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(*magval))
+            for i, axis in enumerate(magval):
+                self.hardiron_calibration[i][0] = min(self.hardiron_calibration[i][0], axis)
+                self.hardiron_calibration[i][1] = max(self.hardiron_calibration[i][1], axis)
+            print("Calibration complete:")
+            print("hardiron_calibration =", self.hardiron_calibration)
+
+    def magnetic(self):
+        _raw_x = self.get_data(OUTX_L_REG)
+        _raw_y = self.get_data(OUTY_L_REG)
+        _raw_z = self.get_data(OUTZ_L_REG)
+
+        return (
+            _raw_x * self._MAG_SCALE,
+            _raw_y * self._MAG_SCALE,
+            _raw_z * self._MAG_SCALE,
+        )
+    
+    def get_data(self, coordinate):
+        buf = bytearray(struct.calcsize('<h'))
+        buf[0],  buf[1] = self.bus.read_i2c_block_data(self._I2C_ADDRESS, coordinate, 2)
+        return struct.unpack_from('<h', buf, 0)[0] * 0.15
+    
+    def normalize(self,magvals):
+        ret = [0, 0, 0]
+        for i, axis in enumerate(magvals):
+            minv, maxv = self.hardiron_calibration[1]
+            axis = min(max(minv, axis), maxv)
+            ret[i] = (axis - minv) * 200 / (maxv - minv) + -100
+        return ret
+
+    def get_heading(self):
+        magvals = self.magnetic()
+        normvals = self.normalize(magvals)
+        compass_heading = int(math.atan2(normvals[1], normvals[0]) * 180.0 / math.pi)
+        compass_heading += 180
+        return compass_heading
+
+
+def main():
+    LIS = LIS2MDL()
+    #LIS.calibrate()
     
 
-def change_config():
-    #bus.write_block_data(ADDRESS, CFG_REG_A, [0b00000000])
-    bus.write_byte_data(ADDRESS, CFG_REG_A, 0b00000010)
-    time.sleep(0.2)
-    print(f"New CFG-A {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_A, 1)[0])[2:].zfill(8)}")
-    print(f"Current STATUS {bin(bus.read_i2c_block_data(ADDRESS, STATUS_REG, 1)[0])[2:].zfill(8)}")
+    print("Getting ready to run")
+    time.sleep(3)
 
-def set_mode(state):
-    #  Get current state
-    reg_val = bus.read_byte_data(ADDRESS, CFG_REG_A, 1)
-    
-    if state == 'idle':
-        # Set bits 0 and 1 to 11
-        reg_val |= (1 << 0)
-        reg_val |= (1 << 1)
-    elif state == 'single':
-        # Set bits 0 and 1 to 01
-        reg_val |= (1 << 0)
-        reg_val &= ~ (1 << 1) & 255        
-    elif state == 'continuous':
-        # Set bits 0 and 1 to 00
-        reg_val &= ~ (1 << 0) & 255 
-        reg_val &= ~ (1 << 1) & 255 
-    else:
-        print("I don't understand that")
-    bus.write_byte_data(ADDRESS, CFG_REG_A, reg_val)
-    print(f"**Set CFG-A to {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_A, 1)[0])[2:].zfill(8)}")
+    i = 0
+    while i < 60:
+        time.sleep(.5)
+        i += 1
+        compass_heading = LIS.get_heading()
+        print("Heading:", compass_heading)
 
-def set_temp_comp(state: bool):
-    #  Get current state
-    reg_val = bus.read_byte_data(ADDRESS, CFG_REG_A, 1)
-    if state == True:
-        # Set bit 7 to 1
-        reg_val |= (1 << 7)
-    else:
-        # Set bit 7 to 0
-        reg_val &= ~ (1 << 7) & 255 
-    bus.write_byte_data(ADDRESS, CFG_REG_A, reg_val)
-    print(f"**Set CFG-A to {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_A, 1)[0])[2:].zfill(8)}")
-
-def reset_reg_a():
-    bus.write_byte_data(ADDRESS, CFG_REG_A, 0x3)
-    print(f"**New CFG-A {bin(bus.read_i2c_block_data(ADDRESS, CFG_REG_A, 1)[0])[2:].zfill(8)}")       
-
-
-def get_data():    
-    buf = bytearray(struct.calcsize('<h'))
-    buf[0],  buf[1] = bus.read_i2c_block_data(ADDRESS, OUTX_L_REG, 2)
-    print(list(buf))
-    print(struct.unpack_from('<h', buf, 0)[0])
-    print(struct.unpack_from('<h', buf, 0)[0] * 0.15)
-    
-    
-
-#get_config()
-#change_config()
-#reset_reg_a()
-#set_mode('single')
-#set_temp_comp(True)
-#
-
-set_mode('continuous')
-
-i = 0
-while i < 1:
-    get_data()
-    time.sleep(1)
-    i +=1
+if __name__ == "__main__":
+    main()
